6a24104a4fb70c994374aff92da7c60834444d97
sharath gowda <sharathgowda.v.96@gmail.com>
2020-03-11 21:22:12 +0530

    Dice-Game Jquery

--- dicegame.js
+++ dicegame.js
@@ -1,46 +1,65 @@
-var num=Math.floor(Math.random()*6)+1;
-var num2=Math.floor(Math.random()*6)+1;
-document.querySelector(".img1").addEventListener("click",randomDiceImage);
-function randomDiceImage(){
-  if(num==1)
-    document.querySelector(".img1").setAttribute("src","dices/Dice-1.png");
-  else if(num==2)
-    document.querySelector(".img1").setAttribute("src","dices/Dice-2.png");
-  else if(num==3)
-    document.querySelector(".img1").setAttribute("src","dices/Dice-3.png");
-  else if(num==4)
-    document.querySelector(".img1").setAttribute("src","dices/Dice-4.png");
-  else if(num==5)
-    document.querySelector(".img1").setAttribute("src","dices/Dice-5.png");
-  else
-    document.querySelector(".img1").setAttribute("src","dices/Dice-6.png");
-}
-document.querySelector(".img2").addEventListener("click",randomDiceImage1);
-function randomDiceImage1(){
-  if(num2==1)
-    document.querySelector(".img2").setAttribute("src","dices/Dice-1.png");
-  else if(num2==2)
-    document.querySelector(".img2").setAttribute("src","dices/Dice-2.png");
-  else if(num2==3)
-    document.querySelector(".img2").setAttribute("src","dices/Dice-3.png");
-  else if(num2==4)
-    document.querySelector(".img2").setAttribute("src","dices/Dice-4.png");
-  else if(num2==5)
-    document.querySelector(".img2").setAttribute("src","dices/Dice-5.png");
-  else
-    document.querySelector(".img2").setAttribute("src","dices/Dice-6.png");
-}
-document.querySelector("button").addEventListener("click",computeResult);
-function computeResult(){
-  if(num>num2)
-    document.querySelector("h1").innerHTML="🚩 Player 1 Wins!";
-  else if(num<num2)
-    document.querySelector("h1").innerHTML="Player 2 Wins! 🚩";
-  else
-    document.querySelector("h1").innerHTML="Draw!";
 
-}
-document.querySelectorAll("button")[1].addEventListener("click",refresh);
-function refresh(){
-  window.location.reload();
-}
+var num=Math.floor(Math.random()*6)+1;
+ var num2=Math.floor(Math.random()*6)+1;
+
+
+   $(".img1").click(function () { 
+     
+    if(num==1){
+          $(".img1").attr("src","dice1.png");
+        }
+        else if(num==2){
+         $(".img1").attr("src","dice2.png");
+       }
+        else if(num==3){
+         $(".img1").attr("src","dice3.png");
+       }
+        else if(num==4){
+         $(".img1").attr("src","dice4.png");
+       }
+       else if(num==5){
+         $(".img1").attr("src","dice5.png");
+       }
+       else{
+         $(".img1").attr("src","dice6.png");
+       }
+   });
+ 
+   
+   $(".img2").click(function () { 
+     
+    if(num2==1){
+          $(".img2").attr("src","dice1.png");
+        }
+        else if(num2==2){
+         $(".img2").attr("src","dice2.png");
+       }
+        else if(num2==3){
+         $(".img2").attr("src","dice3.png");
+       }
+        else if(num2==4){
+         $(".img2").attr("src","dice4.png");
+       }
+       else if(num2==5){
+         $(".img2").attr("src","dice5.png");
+       }
+       else{
+         $(".img2").attr("src","dice6.png");
+       }
+   });
+
+   $("#btn1").click(function(){
+     if(num>num2){
+       $("h1").text("🚩 Player 1 Wins!");
+     }
+     else if(num<num2){
+      $("h1").text("🚩 Player 2 Wins!");
+     }
+     else{
+      $("h1").text("🚩 Draw");
+     }
+   })
+ 
+   $("#btn2").click(function(){
+     window.location.reload();
+   })
